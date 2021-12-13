8193a4a2c828e5d6d996d554d8a62129812b8aae
Alen Jo <alenjo874@gmail.com>
2021-12-13 17:32:45 -0500

    DOM temperature changing color

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let button = document.querySelector(".buttons");
 let searchResults = document.querySelector(".search-results");
-searchResults.style.display = "none";
+// searchResults.style.display = "none";
 
 /* let myFunc = () => {
   const input = document.querySelector(".search-lookup");
@@ -28,12 +28,13 @@ button.addEventListener("click", handleSearch);
 
 const updateValues = (data) => {
   console.log(data);
-  searchResults.style.display = "block";
+
   const input = document.querySelector(".search-lookup");
   const description = document.querySelector(".city-result");
   const temperature = document.querySelector(".temp");
+  const hiddenTab = document.querySelector(".search-results");
 
-  const location = document.querySelector("city");
+  const location = document.querySelector(".city");
   const windGusts = document.querySelector(".wind-gusts");
   const forecast = document.querySelector(".forecast");
   const tempMin = document.querySelector(".temp-min");
@@ -42,10 +43,30 @@ const updateValues = (data) => {
 
   description.innerHTML = `Search results for: ${input.value}`;
   temperature.innerHTML = `${Math.floor(data.main.temp)} &#x2109`;
-  // ask ryan if he sees city state zip location.innerHTML
+  location.innerHTML = `${data.name}, ${data.sys.country}`;
   windGusts.innerHTML = `Wind Gusts: ${Math.floor(data.wind.gust)} mph`;
   forecast.innerHTML = `Forecast: ${data.weather[0].description}`;
   tempMin.innerHTML = `Temp Min: ${Math.floor(data.main.temp_min)} &#x2109`;
   tempMax.innerHTML = `Temp Max: ${Math.floor(data.main.temp_max)} &#x2109`;
   humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
+
+  //temp number changing color based range
+  if (data.main.temp >= 80) {
+    temperature.classList.remove(temperature.classList[1]);
+    temperature.classList.add("red");
+  } else if (data.main.temp >= 64) {
+    temperature.classList.remove(temperature.classList[1]);
+    temperature.classList.add("orange");
+  } else if (data.main.temp >= 33) {
+    temperature.classList.remove(temperature.classList[1]);
+    temperature.classList.add("light-blue");
+  } else {
+    temperature.classList.remove(temperature.classList[1]);
+    temperature.classList.add("blue");
+  }
+
+  // hiding the search tab
+  if (hiddenTab.classList.length > 1) {
+    hiddenTab.classList.remove("none");
+  }
 };
